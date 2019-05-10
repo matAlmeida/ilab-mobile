e65d6ba858c81773e7fa769f48e6b9f829cbf0c9
Matheus Almeida <mat.almeida@live.com>
2019-05-10 11:42:48 -0300

    feat: add constants

---
+++ src/constants/Colors.js
@@ -0,0 +1,16 @@
+const tintColor = '#0D9C60';
+
+export default {
+  tintColor,
+  secondaryColor: '#979797',
+  lightGray: '#E4E4E4',
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+};
