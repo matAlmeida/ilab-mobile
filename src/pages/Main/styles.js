69052bf6d1f85a17a726fce3563198a69d8a60b2
Matheus Almeida <mat.almeida@live.com>
2019-05-11 01:01:58 -0300

    feat: add Main page

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const AppTitle = styled.Text`
+  color: #222;
+  height: 44px;
+  width: 100%;
+  text-align: center;
+  font-size: 24px;
+  font-weight: bold;
+  margin: 40px 0;
+`;
+
+export const ChampionshipsBox = styled.View`
+  flex: 1;
+  align-items: center;
+`;
+
+export const ChampionshipsTitle = styled.Text`
+  color: #222;
+  height: 44px;
+  width: 100%;
+  text-align: center;
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+export const ChampionshipsList = styled.View`
+  flex: 1;
+  width: 100%;
+`;
