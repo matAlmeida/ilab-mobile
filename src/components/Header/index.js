f690999858cfa0228176f5e9cf857bd7d17dfa40
Matheus Almeida <mat.almeida@live.com>
2019-05-13 18:54:12 -0300

    feat: (component) add Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  Container, HeaderButton, HeaderIcon, HeaderTitle,
+} from './styles';
+
+const Header = ({ title, leftIcon }) => (
+  <Container>
+    {!!leftIcon && (
+      <HeaderButton onPress={leftIcon.onPress}>
+        <HeaderIcon name={leftIcon.name} />
+      </HeaderButton>
+    )}
+    <HeaderTitle>{title}</HeaderTitle>
+  </Container>
+);
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  leftIcon: PropTypes.objectOf({ name: PropTypes.string, onPress: PropTypes.func }),
+};
+
+Header.defaultProps = {
+  leftIcon: {},
+};
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,28 @@
+import { Icon } from 'react-native-elements';
+import styled from 'styled-components/native';
+
+import Colors from '~/constants/Colors';
+
+export const Container = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  margin: 40px 0;
+  padding: 0 20px;
+`;
+
+export const HeaderButton = styled.TouchableOpacity`
+  margin-right: 20px;
+`;
+
+export const HeaderIcon = styled(Icon).attrs({
+  color: Colors.tintColor,
+  size: 24,
+})``;
+
+export const HeaderTitle = styled.Text`
+  color: #222;
+  text-align: center;
+  font-size: 24px;
+  font-weight: bold;
+`;
