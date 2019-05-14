9529ebf07a28dc6881d2755575512c21f1ff0463
Matheus Almeida <mat.almeida@live.com>
2019-05-14 00:22:45 -0300

    feat: (router) remove animation between navigation

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,26 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+import { Animated, Easing } from 'react-native';
 
 import Main from '~/pages/Main';
 import Championship from '~/pages/Championship';
+import NewChampionship from '~/pages/NewChampionship';
 
-const Navigator = createSwitchNavigator(
-  { Main, Championship },
-  { order: ['Main', 'Championship'] },
+const Navigator = createStackNavigator(
+  {
+    Main: { screen: Main, navigationOptions: { header: null } },
+    Championship: { screen: Championship, navigationOptions: { header: null } },
+    NewChampionship: { screen: NewChampionship, navigationOptions: { header: null } },
+  },
+  {
+    initialRouteName: 'Main',
+    transitionConfig: () => ({
+      transitionSpec: {
+        duration: 0,
+        easing: Easing.step0,
+        timing: Animated.timing,
+      },
+    }),
+  },
 );
 
 const Routes = createAppContainer(Navigator);
