f025c481e52a92a95f0ff4327cfb061b29cc4957
Matheus Almeida <mat.almeida@live.com>
2019-05-17 02:47:53 -0300

    fix: status bar height

--- src/components/FieldHeader/styles.js
+++ src/components/FieldHeader/styles.js
@@ -1,5 +1,6 @@
 import { Icon } from 'react-native-elements';
 import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 import Colors from '~/constants/Colors';
 
@@ -7,8 +8,7 @@ export const Container = styled.View`
   width: 100%;
   flex-direction: row;
   align-items: center;
-  margin: 0;
-  padding: 20px 20px 20px 0px;
+  padding: ${getStatusBarHeight() + 20}px 20px 20px 0;
   background-color: #fff;
 `;
 
@@ -23,8 +23,9 @@ export const HeaderIcon = styled(Icon).attrs({
 
 export const HeaderTitle = styled.Text`
   color: #222;
-  text-align: center;
+  text-align: left;
   font-size: 24px;
   font-weight: bold;
   margin-left: 20px;
+  flex: 1;
 `;
