235ef07ec3e07fa0e9aed0fa166ad74320f8ba10
Matheus Almeida <mat.almeida@live.com>
2019-11-04 14:04:55 -0300

    fix matrix extraction

--- src/utils/extraction/matrix.js
+++ src/utils/extraction/matrix.js
@@ -1,43 +1,68 @@
-const uda2matrix = (plays, players) => {
-  // const idMap = players.map((player, index) => ({ id: player.id, index }));
-  // const matrix = [];
+/* eslint-disable */
 
-  // for (let i = 0; i < 11; i += 1) {
-  //   const row = [];
+const getPlayersInField = ({ udas }, players) => {
+  const inGamePlayers = [];
+  Object.keys(udas).map(udaId => {
+    const { senderId, receiverId } = udas[udaId];
+    if (
+      inGamePlayers.findIndex(({ id: playerId }) => playerId === senderId) < 0
+    ) {
+      inGamePlayers.push({ id: senderId });
+    }
+    if (
+      inGamePlayers.findIndex(({ id: playerId }) => playerId === receiverId) < 0
+    ) {
+      inGamePlayers.push({ id: receiverId });
+    }
+  });
 
-  //   for (let j = 0; j < 11; j += 1) {
-  //     const column = 0;
-  //     row.push(column);
-  //   }
+  const playersInField = players.filter(
+    ({ id: playerId }) => players.findIndex(({ id }) => id === playerId) >= 0
+  );
 
-  //   matrix.push(row);
-  // }
-
-  // plays.map(play => Object.keys(play.udas)
-  //   .map(key => play.udas[key])
-  //   .map((uda) => {
-  //     const sender = idMap.find(mapped => mapped.id === uda.senderId);
-  //     const receiver = idMap.find(mapped => mapped.id === uda.receiverId);
-
-  //     matrix[sender.index][receiver.index] += 1;
-  //   }));
-
-  // return matrix;
-
-  const ret = { plays, players };
-
-  return JSON.stringify(ret);
+  return playersInField;
 };
 
-const matrix2csv = (matrix) => {
-  // const csv = matrix.reduce((str1, row) => {
-  //   const col = row.reduce((str2, column) => `${str2},${column}`, '');
+const uda2matrix = (plays, players) => {
+  const playerInField = getPlayersInField(plays, players);
 
-  //   return `${str1}${col.substring(1)}\n`;
-  // }, '');
+  const idMap = playerInField.map((player, index) => ({
+    id: player.id,
+    index
+  }));
+  const matrix = [];
 
-  // return csv;
-  const csv = matrix;
+  for (let i = 0; i < 11; i += 1) {
+    const row = [];
+
+    for (let j = 0; j < 11; j += 1) {
+      const column = 0;
+      row.push(column);
+    }
+
+    matrix.push(row);
+  }
+
+  plays.map(play =>
+    Object.keys(play.udas)
+      .map(key => play.udas[key])
+      .map(uda => {
+        const sender = idMap.find(mapped => mapped.id === uda.senderId);
+        const receiver = idMap.find(mapped => mapped.id === uda.receiverId);
+
+        matrix[sender.index][receiver.index] += 1;
+      })
+  );
+
+  return matrix;
+};
+
+const matrix2csv = matrix => {
+  const csv = matrix.reduce((str1, row) => {
+    const col = row.reduce((str2, column) => `${str2},${column}`, "");
+
+    return `${str1}${col.substring(1)}\n`;
+  }, "");
 
   return csv;
 };
